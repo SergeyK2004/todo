6f4f4731ac1ce44adcf2ea98c6949c599a8bf75a
SergeyK2004 <tigra2004@gmail.com>
2024-03-05 16:57:53 +0700

    Список из массива

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,14 @@
 import "./styles/styles.css"
+import { todos } from "./utils/constants"
 
+const contentElement = document.querySelector('.todos__list')
+
+const template = document.querySelector('#todo-item-template') as HTMLTemplateElement
+
+
+todos.forEach(item => {
+  const itemElement = template.content.querySelector('.todo-item').cloneNode(true) as HTMLElement;
+  const title = itemElement.querySelector('.todo-item__text')
+  title.textContent = item;
+  contentElement.prepend(itemElement);
+})
