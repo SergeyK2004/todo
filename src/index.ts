84fbfdc6b12bdf6545f6639b07d7882f5576b68d
SergeyK2004 <tigra2004@gmail.com>
2024-03-06 12:44:47 +0700

    Форма классом

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,26 @@
+import { Form } from "./components/Form"
 import { Item } from "./components/Item"
 import "./styles/styles.css"
 import { todos } from "./utils/constants"
 
 const contentElement = document.querySelector('.todos__list')
 
-const template = document.querySelector('#todo-item-template') as HTMLTemplateElement
+const template = document.querySelector('#todo-item-template') as HTMLTemplateElement;
+const formElement = document.querySelector('.todos__form') as HTMLFormElement;
 
+const todoForm = new Form(formElement, handleSubmitForm)
+
+function handleSubmitForm(data: string) {
+  const todoItem = new Item(template);
+  const itemElement = todoItem.render(data)
+  contentElement.prepend(itemElement);
+  todoForm.clearValue();
+
+}
 
 todos.forEach(item => {
   const todoItem = new Item(template);
   const itemElement = todoItem.render(item)
   contentElement.prepend(itemElement);
 })
+
