9259597c65de0c9f5d0acb494e658d32c3fb763d
SergeyK2004 <tigra2004@gmail.com>
2024-03-06 14:16:35 +0700

    Добавили id и класс для данных

--- src/components/Item.ts
+++ src/components/Item.ts
@@ -1,16 +1,35 @@
+import { IItem } from "../types";
+
 export class Item {
 
     protected itemElement: HTMLElement;
     protected title: HTMLElement;
+    protected _id: string;
 
     constructor (template: HTMLTemplateElement) {
         this.itemElement = template.content.querySelector('.todo-item').cloneNode(true) as HTMLElement;
         this.title = this.itemElement.querySelector('.todo-item__text')
         
     }
+    set id(value: string) {
+        this._id = value;
+    }
 
-    render(item: string) {
-        this.title.textContent = item;
+    get id(): string {
+        return this._id || '';
+    }
+
+    set name(value: string) {
+        this.title.textContent = value;
+    }
+
+    get name(): string {
+        return this.title.textContent || '';
+    }
+
+    render(item: IItem) {
+        this.name = item.name;
+        this.id = item.id
         return this.itemElement;
     }
 
--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,8 @@
 import { Form } from "./components/Form"
 import { Item } from "./components/Item"
+import { ToDoData } from "./components/ToDoData"
 import "./styles/styles.css"
+import { IItem } from "./types"
 import { todos } from "./utils/constants"
 
 const contentElement = document.querySelector('.todos__list')
@@ -10,7 +12,7 @@ const formElement = document.querySelector('.todos__form') as HTMLFormElement;
 
 const todoForm = new Form(formElement, handleSubmitForm)
 
-function handleSubmitForm(data: string) {
+function handleSubmitForm(data: IItem) {
   const todoItem = new Item(template);
   const itemElement = todoItem.render(data)
   contentElement.prepend(itemElement);
@@ -24,3 +26,10 @@ todos.forEach(item => {
   contentElement.prepend(itemElement);
 })
 
+const todoArray = new ToDoData();
+todoArray.items = todos;
+console.log(todoArray.items)
+console.log(todoArray.addItem('Создать класс с данными'))
+console.log(todoArray.items)
+todoArray.removeItem('2')
+console.log(todoArray.items)
