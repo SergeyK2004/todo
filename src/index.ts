87127dd5709261e665502a95ede3440eb487dcda
SergeyK <tigra2004@gmail.com>
2024-03-17 08:54:09 +1100

    Карточки чистым js

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,19 @@
 import "./styles/styles.css"
+import { todos } from "./utils/constants"
 
+const contentElement = document.querySelector('.todos__list')
+
+const template = document.querySelector('#todo-item-template') as HTMLTemplateElement
+
+function createItem(name: string) {
+    const itemElement = template.content.querySelector('.todo-item').cloneNode(true) as HTMLElement;
+    const title = itemElement.querySelector('.todo-item__text')
+    title.textContent = name;
+    return itemElement;
+
+}
+
+todos.forEach(item => {
+  const itemElement = createItem(item)
+  contentElement.prepend(itemElement);
+})
